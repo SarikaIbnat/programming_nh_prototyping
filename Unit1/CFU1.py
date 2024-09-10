d8684529408a4ba1f0132d4442fc76f9367fa781
Sarika Ibnat <144722657+SarikaIbnat@users.noreply.github.com>
2024-09-10 11:56:05 -0400

    Update CFU1.py

--- Unit1/CFU1.py
+++ Unit1/CFU1.py
@@ -1,3 +1,9 @@
 user_name= input("What's your First name?")
 user_last= input("What's your Last name?")
-print("Hi nice to have you here" + course)
+
+course= (user_name + user_last)
+
+city= input("Which city do you live in?")
+hobby= "How do you like to spend your free time?"
+user= (course + city + hobby)
+print("Hi nice to have you here" + course + "I heard it's lovely up there!" + "That's really cool! I like biking by the way")
